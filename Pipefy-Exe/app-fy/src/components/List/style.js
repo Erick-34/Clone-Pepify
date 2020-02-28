38d65d106c45eddd013605fdb7134121176380b5
Erick-24 <erickoliveira2060@gmail.com>
2020-02-28 12:42:24 -0300

    subindo

---
+++ Pipefy-Exe/app-fy/src/components/List/style.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+   padding: 0 15px;
+   height: 100px;
+   flex: 0 0 320px;
+   opacity: ${props => props.done ? 0.6 : 1};
+   
+
+   & + div {
+       border-left: 1px solid rgba(0, 0, 0, 0.05);
+   }
+
+   header {
+       display: flex;
+       justify-content: space-between;
+       aling-items: center;
+       heigth: 42px;
+
+       h2{
+         font-weight: 500;
+         font-size:15px;
+         padding: 0 10px;
+       }
+       button {
+           width: 42px;
+           height: 42px;
+           border-radius:20px;
+           background: #3b5bfd;
+           border 0;
+           cursor: pointer;
+       }
+       ul {
+           margin-top:30px;
+       }
+   }
+`;
